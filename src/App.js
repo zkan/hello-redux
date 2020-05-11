8e808d1420a5a2dad25fd37f443458f7f628bbc8
zkan <zkan.cs@gmail.com>
2020-05-11 23:31:17 +0700

    :lipstick: Show only counter and add button :bear:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const Counter = () => <div>5</div>
+
+const AddButton = () => <button>+</button>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Counter />
+      <AddButton />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
