74e247445667beebc503c53b4aeaa7f89ac39a95
zkan <zkan.cs@gmail.com>
2020-05-11 23:47:31 +0700

    :sparkles: Update counter state in store by dispatching action :bear:

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import './App.css'
 
-const Counter = () => <div>5</div>
+const Counter = (props) => <div>{props.ctr}</div>
 
-const AddButton = () => <button>+</button>
+const AddButton = (props) => <button onClick={props.onIncrementCounter}>+</button>
 
 function App() {
   return (
     <div className='App'>
-      <Counter />
-      <AddButton />
+      <ConnectedCounter />
+      <ConnectedAddButton />
     </div>
   )
 }
+
+const mapStateToProps = (state) => {
+  return { ctr: state.counter }
+}
+
+const ConnectedCounter = connect(mapStateToProps)(Counter)
+
+const mapDisptachToProps = (dispatch) => {
+  return {
+    onIncrementCounter: () => dispatch({ type: 'INC' })
+  }
+}
+
+const ConnectedAddButton = connect(null, mapDisptachToProps)(AddButton)
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+
+const initialState = {
+  counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INC':
+      return {
+        counter: state.counter + 1
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
